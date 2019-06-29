7159e60f19079f2b852ad8fa087469eca532cd54
DiaoSu <723714386@qq.com>
2019-06-29 15:24:40 +0800

    调整界面2.0

--- app/src/main/java/com/dannydiao/androidsipdemo/LoginActivity.java
+++ app/src/main/java/com/dannydiao/androidsipdemo/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.dannydiao.androidsipdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -16,41 +20,56 @@ import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private final OkHttpClient client = new OkHttpClient();
-
-
-    Button login = findViewById(R.id.login_button);
-    Button register = findViewById(R.id.register_button);
-    TextView accout_textview = findViewById(R.id.account_input);
-    TextView password_textview = findViewById(R.id.key_input);
-
+    private OkHttpClient client = new OkHttpClient();
     String accout;
     String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login = findViewById(R.id.login_button);
+        Button register = findViewById(R.id.register_button);
+        TextView accout_textview = findViewById(R.id.account_input);
+        TextView password_textview = findViewById(R.id.key_input);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                accout = accout_textview.toString();
-                password = password_textview.toString();
-
-
+                accout = accout_textview.getText().toString();
+                password = password_textview.getText().toString();
                 RequestBody formBody = new FormBody.Builder()
-                        .add("name",)
+                        .add("userID", accout)
+                        .add("password", password)
                         .build();
                 Request request = new Request.Builder()
-                        .url("https://diaosudev.cn:6666/login")
-                        .post(formBody)
+                        .url("https://diaosudev.cn:6666/login?userID=" + accout + "&password=" + password)
+                        .get()
                         .build();
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
 
-                try {
-                    Response response = client.newCall(request).execute();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                }
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d("响应", response.body().string());
+                    }
+                });
+
+
+
+
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), RegisterActivity.class);
+                startActivity(intent);
+
+            }
         });
     }
 }
--- app/src/main/java/com/dannydiao/androidsipdemo/RegisterActivity.java
+++ app/src/main/java/com/dannydiao/androidsipdemo/RegisterActivity.java
@@ -1,13 +1,91 @@
 package com.dannydiao.androidsipdemo;
 
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView name_textview = findViewById(R.id.name_input);
+        TextView password_textview = findViewById(R.id.key_input);
+        TextView ID_textview = findViewById(R.id.ID_input);
+        Button register = findViewById(R.id.register_button);
+        Button back = findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = name_textview.getText().toString();
+                String password = password_textview.getText().toString();
+                String ID = ID_textview.getText().toString();
+
+                RequestBody formBody = new FormBody.Builder()
+                        .add("name", name)
+                        .add("userID", ID)
+                        .add("password", password)
+                        .build();
+
+                final Request request = new Request.Builder()
+                        .url("https://diaosudev.cn:6666/register")
+                        .post(formBody)
+                        .build();
+                OkHttpClient client = new OkHttpClient();
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Looper.prepare();
+                        if (response.body().string().equals("success!")) {
+                            Toast.makeText(getApplication(), "注册成功！", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(getApplication(), "网络出问题啦233请检查网络", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        Looper.loop();
+
+                    }
+                });
+
+
+
+
+
+
+            }
+        });
+
     }
 }
