2c1b952afa95f6e9f080dc044609e81e2b162427
DiaoSu <723714386@qq.com>
2019-07-04 10:01:38 +0800

    写完recyclerview

--- app/src/main/java/com/dannydiao/androidsipdemo/HallActivity.java
+++ app/src/main/java/com/dannydiao/androidsipdemo/HallActivity.java
@@ -2,12 +2,39 @@ package com.dannydiao.androidsipdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HallActivity extends AppCompatActivity {
+
+    List<String> name = new ArrayList<>();
+    List<String> ID = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hall);
+        name.add("测试");
+        name.add("测试");
+        name.add("测试");
+        name.add("测试");
+        ID.add("测试");
+        ID.add("测试");
+        ID.add("测试");
+        ID.add("测试");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        FriendsAdapter friendsAdapter = new FriendsAdapter(name, ID);
+        recyclerView.setAdapter(friendsAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext()
+                , DividerItemDecoration.VERTICAL));
+
     }
 }
